9831728873b9aa44e8033d1381a380aaeb4e0a30
Christofer-A <cazadian@ucsd.edu>
2024-07-02 10:25:45 -0700

    Delete ChoreHelper directory

--- ChoreHelper/InvalidSplitException.java
+++
@@ -1,6 +0,0 @@
-public class InvalidSplitException extends Exception {
-    public InvalidSplitException(String name) {
-        //Creates a new Exception for inputting an invalid Split time
-        super(name);
-    }
-}
--- ChoreHelper/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //Starts the program
-        GUI g = new GUI();
-    }
-}
